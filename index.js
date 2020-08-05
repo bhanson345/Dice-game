e8d8a1d13f3db6468a933ab5f92731db019e4d69
Brett <bretth345@gmail.com>
2020-08-05 00:01:14 -0500

    Add files via upload
    Added a function and onclick event

--- index.js
+++ index.js
@@ -1,14 +1,21 @@
-var randomNumber1 = (Math.random() * 6 | 0) + 1;
-var randomNumber2 = (Math.random() * 6 | 0) + 1;
-var playerWins = document.querySelector("h1");
-randomNumber1 = document.querySelector(".img1").src = "images/dice" + randomNumber1 + ".png";
+function randomNumber() {
+    var randomNumber1 = (Math.random() * 6 | 0) + 1;
+    var randomNumber2 = (Math.random() * 6 | 0) + 1;
 
-randomNumber2 = document.querySelector(".img2").src = "images/dice" + randomNumber2 + ".png";
+    var playerWins = document.querySelector("h1");
 
-if (randomNumber1 > randomNumber2) {
-    playerWins.innerText = "Player one Wins!";
-} else if (randomNumber1 < randomNumber2) {
-    playerWins.innerText = "Player Two Wins!";
-} else {
-    playerWins.innerText = "Draw!";
+    randomNumber1 = document.querySelector(".img1").src = "images/dice" + randomNumber1 + ".png";
+
+    randomNumber2 = document.querySelector(".img2").src = "images/dice" + randomNumber2 + ".png";
+
+
+
+    if (randomNumber1 > randomNumber2) {
+        playerWins.innerText = "Player one Wins!";
+    } else if (randomNumber1 < randomNumber2) {
+        playerWins.innerText = "Player Two Wins!";
+    } else {
+        playerWins.innerText = "Draw!";
+    }
+
 }
